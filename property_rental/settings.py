463dcda8c635c46a70afc96f532fef632e2e2074
Shivam Tyagi <shivamtyagi0035@gmail.com>
2026-09-14 18:33:16 +0530

    Configure ALLOWED_HOSTS from environment

--- property_rental/settings.py
+++ property_rental/settings.py
@@ -26,7 +26,7 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 
 # Application definition
